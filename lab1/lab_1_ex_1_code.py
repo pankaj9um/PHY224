3f4b8ca4a20aef77ce8c0b94548a619be28aafa9
Pankaj Patil <Pankaj.Patil@autodesk.com>
2021-09-23 18:45:50 -0400

    we need potentiometer values as well

--- lab1/lab_1_ex_1_code.py
+++ lab1/lab_1_ex_1_code.py
@@ -28,64 +28,74 @@ def current_uncertainity(current):
 def linear_model_function(x, a, b):
     return a*x + b 
 
-# filename
-filename = "100k.csv"
-
-# load the csv file as txt
-measured_voltages, measured_currents = np.loadtxt(filename, 
-                                                skiprows=1, 
-                                                usecols=(0,1), 
-                                                delimiter=",", 
-                                                unpack=True)
-    
-# create error array for the voltage
-voltage_errors = np.ones_like(measured_voltages) * voltage_uncertainity
-
-# create error array for the current
-current_errors = np.vectorize(current_uncertainity)(measured_currents)
-
-# do the curve fitting
-popt, pcov = optim.curve_fit(linear_model_function, 
-                             measured_voltages, 
-                             measured_currents, 
-                             absolute_sigma=True, 
-                             sigma=current_errors)
-
-print("linear fit model gives a=%.2f, b=%.2f" % (popt[0], popt[1]))
-pvar = np.diag(pcov)
-
-# new figure for this file
-plt.figure(figsize=(16, 10))
-plt.style.use("seaborn-whitegrid")
+def analyse_file(filename, title):
+    # load the csv file as txt
+    measured_voltages, measured_currents = np.loadtxt(filename, 
+                                                    skiprows=1, 
+                                                    usecols=(0,1), 
+                                                    delimiter=",", 
+                                                    unpack=True)
         
-# plot the error bar chart
-plt.errorbar(measured_voltages,
-             measured_currents, 
-             yerr=current_errors, 
-             marker="o",
-             label="measured currents",
-             capsize=2,
-             ls="")
+    # create error array for the voltage
+    voltage_errors = np.ones_like(measured_voltages) * voltage_uncertainity
 
-# plot the fitted curve
-# add 0 to the measured data set
-measured_voltages_with_0 = np.append(measured_voltages, 0)
-plt.plot(measured_voltages_with_0, 
-         linear_model_function(measured_voltages_with_0, popt[0], popt[1]),
-         label='$I = V/R$ (fitted linear curve)')
+    # create error array for the current
+    current_errors = np.vectorize(current_uncertainity)(measured_currents)
 
-# legend and title
-plt.title("Current vs Voltage (Resistor 100 kiloohm)")
-plt.xlabel("Voltage (V)")
-plt.ylabel("Current (mA)")
-plt.legend(loc="upper left")
-plt.savefig("lab_1_ex_1_plot.png")
+    # do the curve fitting
+    popt, pcov = optim.curve_fit(linear_model_function, 
+                                measured_voltages, 
+                                measured_currents, 
+                                absolute_sigma=True, 
+                                sigma=current_errors)
 
-chi2r = chi2reduced(measured_currents,
-                    linear_model_function(measured_voltages, popt[0], popt[1]),
-                    current_errors,
-                    2)
+    pvar = np.diag(pcov)
 
-print("fitted (average) resistance = %.3f kiloohm" % (1/popt[0]))
-print("error in fitted resistance = %.5f kiloohm" % np.sqrt(pvar[0]))
-print("model chi2r = %.3f" % chi2r) 
+    # new figure for this file
+    plt.figure(figsize=(16, 10))
+    plt.style.use("seaborn-whitegrid")
+            
+    # plot the error bar chart
+    plt.errorbar(measured_voltages,
+                measured_currents, 
+                yerr=current_errors, 
+                marker="o",
+                label="measured currents",
+                capsize=2,
+                ls="")
+
+    # plot the fitted curve
+    # add 0 to the measured data set
+    measured_voltages_with_0 = np.append(measured_voltages, 0)
+    plt.plot(measured_voltages_with_0, 
+            linear_model_function(measured_voltages_with_0, popt[0], popt[1]),
+            label='$I = V/R$ (fitted linear curve)')
+
+    # legend and title
+    plt.title("Current vs Voltage (Resistor = %s)" % title)
+    plt.xlabel("Voltage (V)")
+    plt.ylabel("Current (mA)")
+    plt.legend(loc="upper left")
+    plt.savefig("lab_1_ex_1_plot_%s.png" % filename[:-4])
+
+    chi2r = chi2reduced(measured_currents,
+                        linear_model_function(measured_voltages, 
+                                              popt[0], 
+                                              popt[1]),
+                        current_errors,
+                        2)
+
+    print("filename %s" % filename)
+    print("\tlinear fit model gives a=%.2f, b=%.2f" % (popt[0], popt[1]))
+    print("\tfitted (average) resistance = %.3f kiloohm" % (1/popt[0]))
+    print("\terror in fitted resistance = %.5f kiloohm" % np.sqrt(pvar[0]))
+    print("\tmodel chi2r = %.3f" % chi2r)
+
+# filename
+file_titles ={
+    "100k.csv": "100 kiloohm",
+    "Potentiometer.csv": "potentiometer"
+}
+
+for filename, title in file_titles.items():
+    analyse_file(filename, title)
