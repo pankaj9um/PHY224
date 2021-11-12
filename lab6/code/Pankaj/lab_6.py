e559eeac6557d3b936e89ab50f970806c65c12bb
Pankaj Patil <Pankaj.Patil@autodesk.com>
2021-11-11 23:01:19 -0500

    update report 6

--- lab6/code/Pankaj/lab_6.py
+++ lab6/code/Pankaj/lab_6.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-@author: Pankaj Patil
-"""
 
 import math
 import statslab as utils
@@ -10,25 +7,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-frequency_uncertainity = 1 #1MHz
+# constants for this exercise
+
+# constant uncertainity in frequency measurement in MHz
+frequency_uncertainity = 2
+
+# electron charge in Coulomb
 e = 1.602 * 10 ** (-19)
+
+# electron mass ini kg
 m = 9.109 * 10 ** (-31)
 
+# permeability
 mu = 4*math.pi* (10**(-7)) 
+
+# number of turns of the external coils
 n = 320
+
+# radius of the coils in mm
 R = 70
 
-frequency_multiplier = 1 #MHz
-
+# linear model function without y-intercept
 def model_function(currrent, slope):
     return currrent * slope
 
-# import the first setup for Voltage data
-#files = ["../../data/Highest_N_Data.csv", "../../data/Medium_N_Data.csv"]
-files = ["../../data/Highest_N_Data.csv"]
+# import the measured data from data files
+files = ["../../data/Highest_N_Data.csv", "../../data/Medium_N_Data.csv"]
+
 measured_currents = np.array([])
 measured_frequency = np.array([])
 
+# iterate over all the files to collect measured currrents and frequency
 for f in files:
     c, f = utils.read_data(f,
                            usecols=(2, 1),
@@ -37,40 +46,65 @@ for f in files:
     measured_currents = np.append(measured_currents, c)
     measured_frequency = np.append(measured_frequency, f)
     
+# measure current correction as the current is split between two coils.
 measured_currents = measured_currents/2
-measured_frequency = measured_frequency * frequency_multiplier # conver to SI units
+
+# frequency errors
 frequency_errors = np.ones_like(measured_frequency) * frequency_uncertainity
 
+# compute the measured magnetic field in Tesla
 measured_magnetic_field = (4/5)**(3/2) * mu * n * measured_currents / R
 
+# compute the gyromagnetic ratio for each measurement
+gamma_measured = 2*math.pi*measured_frequency/measured_magnetic_field
+
+# compute the Lande g factor
+g_measured = gamma_measured / (e / (2 * m)) * 10 ** 6
+
+# standard devation in g factor
+std_g = np.std(g_measured)
+print("Standard deviation of measured Lande g Factor = %.2f" % std_g)
+
+
+# fit the data to our model function
 popt, pstd = utils.fit_data(model_function, 
                           measured_magnetic_field, 
                           measured_frequency, 
                           frequency_errors)
 
 slope = popt[0]
-print("slope = %.4f" % slope)
-# print("y_intercept = %.2f" %  popt[1])
 
-c_min = np.min(measured_magnetic_field)
-c_max = np.max(measured_magnetic_field)
+print("Slope of the line (frequency vs magnetic field) = %.4f" % slope)
 
-gamma_measured = 2*math.pi*measured_frequency/measured_magnetic_field
-g_measured = gamma_measured / (e / (2 * m)) * 10 ** 6
-std_gamma = np.std(g_measured)
-print("std of gamma = %.2f" % std_gamma)
+# compute gamma (gyromagnetic  ratio) using fit data
+gamma = 2*math.pi*slope
+gamma_var = 2*math.pi* pstd[0]
+print("gamma (gyromagnetic  ratio) = %.2f +/- %.2f rad/s/T" % (gamma, gamma_var))
 
-magnetic_field_for_prediction = np.linspace(c_min, c_max, 1000)
+# compute Lange g Factor
+g = gamma / (e / (2 * m)) * 10**6
+gvar = gamma_var / (e / (2 * m)) * 10**6
+print("Lande g Factor= %.2f +/- %.2f" % (g, gvar))
 
+# prepare data for prediction using the curve fit slope value
+m_min = np.min(measured_magnetic_field)
+m_max = np.max(measured_magnetic_field)
+magnetic_field_for_prediction = np.linspace(m_min, m_max, 1000)
+
+# compute the predicted frequencies
 predicted_fequency = model_function(magnetic_field_for_prediction, 
-                                            popt[0])
+                                    slope)
 
+# compute chi2r
 chi2r_curve_fit = utils.chi2reduced(measured_frequency, 
-                              model_function(measured_magnetic_field, 
-                              popt[0]), #
-                              frequency_errors, 
-                              1)
+                                    model_function(measured_magnetic_field, 
+                                                   slope),
+                                    frequency_errors, 
+                                    1)
 
+print("chi2reduced = %.4f" % chi2r_curve_fit)
+
+# plot measured frequency vs magnetic field, with the curve fit
 plot_data = utils.plot_details("Electron Spin Resonance \
                                (Resonance Frequency vs Magnetic Field)")
 plot_data.errorbar_legend("Measured Frequency")
@@ -88,22 +122,19 @@ plot_data.chi2_reduced(chi2r_curve_fit)
 # plot the data
 utils.plot(plot_data)
 
+# save the plot
 plt.savefig("lab6_freq_vs_magnetic_field.png")
-print("chi2reduced = %.4f" % chi2r_curve_fit)
 
 fig  = plt.figure(figsize=(16, 10))
 fig.tight_layout()
 plt.style.use("seaborn-whitegrid")
-#xdata = np.arange(0, len(measured_frequency))
-plt.errorbar(measured_frequency, g_measured, marker="o", 
-             yerr=std_gamma*np.ones_like(g_measured), fmt=" ")
+plt.errorbar(measured_frequency, g_measured, marker="o",
+             yerr=std_g*np.ones_like(g_measured), fmt=" ", 
+             label="Measured Lande g Factor")
 plt.xlabel("Frequency(MHz)")
-plt.ylabel("g Factor")
-plt.title("g Factor vs.  Resonance Frequency")
+plt.ylabel("Lande g Factor")
+plt.title("Lande g Factor vs. Resonance Frequency (MHz)")
 plt.legend(loc="upper left")
 
-gamma = 2*math.pi*slope
-print("gamma = %.2f" % gamma)
-
-g = gamma / (e / (2 * m)) * 10**6
-print("g = %.4f" % g)
+# save the plot
+plt.savefig("lab6_g_factor_vs_freq.png")
